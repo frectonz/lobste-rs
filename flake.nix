2257bec72d8a36ca3ca8c75654066efeadf78884
frectonz <fraol0912@gmail.com>
2023-08-01 08:59:03 +0300

    create a default build

--- flake.nix
+++ flake.nix
@@ -9,17 +9,40 @@
         flake-utils.follows = "flake-utils";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        craneLib = crane.lib.${system};
+        src = craneLib.cleanCargoSource ./.;
+
+        commonArgs = { inherit src; };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        bin = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
       in
       with pkgs;
       {
+        packages = {
+          inherit bin;
+          default = bin;
+        };
+
         devShells.default = mkShell {
           buildInputs = [
             rust-bin.stable.latest.default
