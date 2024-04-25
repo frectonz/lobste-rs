19767d33cc85a6db73f6ee7387d20d61d5054185
frectonz <fraol0912@gmail.com>
2024-04-26 02:48:20 +0300

    don't have a struct just extract the needed field

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::io;
 
-use color_eyre::Result;
+use color_eyre::{eyre::eyre, Result};
 use crossterm::{
     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use reqwest::blocking::Client;
-use serde::Deserialize;
 use tui::{
     backend::{Backend, CrosstermBackend},
     style::{Modifier, Style},
@@ -15,136 +14,6 @@ use tui::{
     widgets::{Block, List, ListItem, Paragraph},
     Frame, Terminal,
 };
-
-#[allow(dead_code)]
-
-type Stories = Vec<Story>;
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Story {
-    #[serde(rename = "short_id")]
-    pub short_id: String,
-    #[serde(rename = "short_id_url")]
-    pub short_id_url: String,
-    #[serde(rename = "created_at")]
-    pub created_at: String,
-    pub title: String,
-    pub url: String,
-    pub score: i64,
-    pub flags: i64,
-    #[serde(rename = "comment_count")]
-    pub comment_count: i64,
-    pub description: String,
-    #[serde(rename = "description_plain")]
-    pub description_plain: String,
-    #[serde(rename = "comments_url")]
-    pub comments_url: String,
-    #[serde(rename = "submitter_user")]
-    pub submitter_user: SubmitterUser,
-    pub tags: Vec<String>,
-}
-
-impl Story {
-    fn url(&self) -> &str {
-        if self.url.is_empty() {
-            &self.short_id_url
-        } else {
-            &self.url
-        }
-    }
-
-    fn url_span(&self) -> text::Span {
-        text::Span::styled(
-            self.url(),
-            Style::default()
-                .fg(tui::style::Color::Blue)
-                .add_modifier(tui::style::Modifier::UNDERLINED),
-        )
-    }
-
-    fn title_span(&self, selected: bool) -> text::Span {
-        text::Span::styled(
-            &self.title,
-            Style::default()
-                .add_modifier(Modifier::BOLD)
-                .fg(if selected {
-                    tui::style::Color::Green
-                } else {
-                    tui::style::Color::White
-                }),
-        )
-    }
-
-    fn score_span(&self) -> text::Span {
-        text::Span::styled(
-            format!("⧋ {: <4}", self.score),
-            Style::default().fg(tui::style::Color::Yellow),
-        )
-    }
-}
-
-struct StoryWidget<'a> {
-    story: &'a Story,
-    selected: bool,
-}
-
-impl<'a> StoryWidget<'a> {
-    fn new((story, selected): (&'a Story, bool)) -> Self {
-        Self { story, selected }
-    }
-}
-
-impl<'a> Into<ListItem<'a>> for StoryWidget<'a> {
-    fn into(self) -> ListItem<'a> {
-        let selected_indicator = if self.selected { "► " } else { "  " };
-
-        let span = text::Spans::from(vec![
-            selected_indicator.into(),
-            self.story.score_span(),
-            " ".into(),
-            self.story.title_span(self.selected),
-            " ".into(),
-            self.story.url_span(),
-        ]);
-
-        ListItem::new(span)
-    }
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct SubmitterUser {
-    pub username: String,
-    #[serde(rename = "created_at")]
-    pub created_at: String,
-    #[serde(rename = "is_admin")]
-    pub is_admin: bool,
-    pub about: String,
-    #[serde(rename = "is_moderator")]
-    pub is_moderator: bool,
-    pub karma: Option<i64>,
-    #[serde(rename = "avatar_url")]
-    pub avatar_url: String,
-    #[serde(rename = "invited_by_user")]
-    pub invited_by_user: Option<String>,
-    #[serde(rename = "github_username")]
-    pub github_username: Option<String>,
-    #[serde(rename = "twitter_username")]
-    pub twitter_username: Option<String>,
-    #[serde(rename = "keybase_signatures")]
-    #[serde(default)]
-    pub keybase_signatures: Vec<KeybaseSignature>,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct KeybaseSignature {
-    #[serde(rename = "kb_username")]
-    pub kb_username: String,
-    #[serde(rename = "sig_hash")]
-    pub sig_hash: String,
-}
 
 const BANNER: &str = r#"
  ████           █████              █████                                 
@@ -159,9 +28,124 @@ const BANNER: &str = r#"
                                                                          
 "#;
 
+pub struct Story(serde_json::Value);
+
+impl Story {
+    fn url(&self) -> Option<&str> {
+        let serde_json::Value::Object(ref story) = self.0 else {
+            return None;
+        };
+
+        let serde_json::Value::String(url) = story.get("url")? else {
+            return None;
+        };
+        let serde_json::Value::String(short_id_url) = story.get("short_id_url")? else {
+            return None;
+        };
+        if url.is_empty() {
+            Some(short_id_url)
+        } else {
+            Some(url)
+        }
+    }
+
+    fn url_span(&self) -> Option<text::Span> {
+        self.url().map(|url| {
+            text::Span::styled(
+                url,
+                Style::default()
+                    .fg(tui::style::Color::Blue)
+                    .add_modifier(tui::style::Modifier::UNDERLINED),
+            )
+        })
+    }
+
+    fn title(&self) -> Option<&str> {
+        let serde_json::Value::Object(ref story) = self.0 else {
+            return None;
+        };
+
+        let serde_json::Value::String(title) = story.get("title")? else {
+            return None;
+        };
+
+        Some(title)
+    }
+
+    fn title_span(&self, selected: bool) -> Option<text::Span> {
+        self.title().map(|title| {
+            text::Span::styled(
+                title,
+                Style::default()
+                    .add_modifier(Modifier::BOLD)
+                    .fg(if selected {
+                        tui::style::Color::Green
+                    } else {
+                        tui::style::Color::White
+                    }),
+            )
+        })
+    }
+
+    fn score(&self) -> Option<i64> {
+        let serde_json::Value::Object(ref story) = self.0 else {
+            return None;
+        };
+
+        let serde_json::Value::Number(score) = story.get("score")? else {
+            return None;
+        };
+
+        score.as_i64()
+    }
+
+    fn score_span(&self) -> Option<text::Span> {
+        self.score().map(|score| {
+            text::Span::styled(
+                format!("⧋ {: <4}", score),
+                Style::default().fg(tui::style::Color::Yellow),
+            )
+        })
+    }
+}
+
+struct StoryWidget<'a> {
+    story: &'a Story,
+    selected: bool,
+}
+
+impl<'a> StoryWidget<'a> {
+    fn new(story: &'a Story, selected: bool) -> Self {
+        Self { story, selected }
+    }
+
+    fn to_item(&self) -> Option<ListItem<'a>> {
+        let selected_indicator = if self.selected { "► " } else { "  " };
+
+        let span = text::Spans::from(vec![
+            selected_indicator.into(),
+            self.story.score_span()?,
+            " ".into(),
+            self.story.title_span(self.selected)?,
+            " ".into(),
+            self.story.url_span()?,
+        ]);
+
+        Some(ListItem::new(span))
+    }
+}
+
+fn get_stories(stories: serde_json::Value) -> Option<Vec<Story>> {
+    let serde_json::Value::Array(stories) = stories else {
+        return None;
+    };
+
+    Some(stories.into_iter().map(Story).collect())
+}
+
 struct App {
     client: Client,
-    stories: Stories,
+    stories: Vec<Story>,
     terminal: Terminal<CrosstermBackend<io::Stdout>>,
     selected_story_index: usize,
     page: usize,
@@ -172,7 +156,7 @@ impl App {
         let client = Client::builder().build()?;
         let mut terminal = App::init_screen()?;
 
-        let stories: Stories = client
+        let stories = client
             .get("https://lobste.rs/newest.json")
             .send()?
             .json()
@@ -180,6 +164,7 @@ impl App {
                 reset_terminal(&mut terminal);
                 e
             })?;
+        let stories = get_stories(stories).ok_or(eyre!("couldn't find stories"))?;
 
         Ok(Self {
             client,
@@ -224,8 +209,8 @@ impl App {
                     }
                     KeyCode::Enter => {
                         if let Some(story) = self.stories.get(self.selected_story_index) {
-                            if let Err(e) = open::that(story.url.as_str()) {
-                                eprintln!("Error opening url: {}", e);
+                            if story.url().and_then(|url| open::that(url).ok()).is_none() {
+                                eprintln!("Error opening url");
                             }
                         }
                     }
@@ -233,21 +218,27 @@ impl App {
                         // breaks after page 5
                         if self.page < 5 {
                             self.page += 1;
-                            self.stories = self
+                            let stories = self
                                 .client
                                 .get(format!("https://lobste.rs/newest/page/{}.json", self.page))
                                 .send()?
                                 .json()?;
+
+                            self.stories =
+                                get_stories(stories).ok_or(eyre!("couldn't find stories"))?;
                         }
                     }
                     KeyCode::Left => {
                         if self.page > 1 {
                             self.page -= 1;
-                            self.stories = self
+                            let stories = self
                                 .client
                                 .get(format!("https://lobste.rs/newest/page/{}.json", self.page))
                                 .send()?
                                 .json()?;
+
+                            self.stories =
+                                get_stories(stories).ok_or(eyre!("couldn't find stories"))?;
                         }
                     }
                     _ => {}
@@ -256,13 +247,12 @@ impl App {
         }
     }
 
-    fn draw_stories<B: Backend>(f: &mut Frame<B>, stories: &Stories, index: usize, page: usize) {
+    fn draw_stories<B: Backend>(f: &mut Frame<B>, stories: &[Story], index: usize, page: usize) {
         let items: Vec<ListItem> = stories
             .iter()
             .enumerate()
-            .map(|(i, s)| (s, i == index))
-            .map(StoryWidget::new)
-            .map(Into::into)
+            .map(|(i, s)| StoryWidget::new(s, i == index))
+            .filter_map(|s| s.to_item())
             .collect();
 
         let layout = tui::layout::Layout::default()
